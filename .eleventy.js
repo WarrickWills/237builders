17b591ea558106a359685af230e3fab6fec2edff
Warrick Wills <warrick.j@live.com>
2026-05-03 01:53:09 +1200

    try fix

--- .eleventy.js
+++ .eleventy.js
@@ -131,5 +131,6 @@ module.exports = function (eleventyConfig) {
 			data: "_data", // Global data files directory
 		},
 		htmlTemplateEngine: "njk", // Nunjucks for HTML templates
+		pathPrefix: "/237builders/",
 	};
 };
