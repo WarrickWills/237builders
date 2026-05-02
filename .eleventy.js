889ea4cf580efa0a08cf8f11837058dda73eb9e7
Warrick Wills <warrick.j@live.com>
2026-05-03 01:41:09 +1200

    Ged rid of blog

--- .eleventy.js
+++ .eleventy.js
@@ -76,7 +76,7 @@ module.exports = function (eleventyConfig) {
 	// ═════════════════════════════════════════════════════════════════════════
 
 	eleventyConfig.addPassthroughCopy("./src/assets"); // Static assets
-	eleventyConfig.addPassthroughCopy("./src/admin"); // CMS admin files
+
 	eleventyConfig.addPassthroughCopy("./src/_redirects"); // Redirect rules
 
 	// ═════════════════════════════════════════════════════════════════════════
